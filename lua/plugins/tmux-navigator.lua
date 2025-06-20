3df7cf97f843c5ea02b885d2be6e0374d5faad42
joaquin <joacoangelino@gmail.com>
2025-06-20 02:57:54 -0300

    feat tmux navigator

---
+++ lua/plugins/tmux-navigator.lua
@@ -0,0 +1,11 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    -- keys = {
+    --     { "<C-h>",  "<cmd><C-U>TmuxNavigateLeft<CR>",     mode = { "n", "t" } },
+    --     { "<C-j>",  "<cmd><C-U>TmuxNavigateDown<CR>",     mode = { "n", "t" } },
+    --     { "<C-k>",  "<cmd><C-U>TmuxNavigateUp<CR>",       mode = { "n", "t" } },
+    --     { "<C-l>",  "<cmd><C-U>TmuxNavigateRight<CR>",    mode = { "n", "t" } },
+    --     { "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>", mode = { "n", "t" } },
+    -- },
+}
